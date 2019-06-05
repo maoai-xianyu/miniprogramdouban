6fd99105cdd66b62e9353b3302e9dbff3a7e1373
zhangkun@boxfish.cn <194264514@qq.com>
2019-06-05 16:20:00 +0800

    豆瓣搜索栏

---
+++ components/searchbar/searchbar.js
@@ -0,0 +1,28 @@
+// components/searchbar/searchbar.js
+Component({
+    /**
+     * 组件的属性列表
+     */
+    properties: {
+
+        isNavigator: {
+            type: Boolean,
+            vales: false
+        }
+
+    },
+
+    /**
+     * 组件的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 组件的方法列表
+     */
+    methods: {
+
+    }
+})
