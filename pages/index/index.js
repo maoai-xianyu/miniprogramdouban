f54a34703d2f303dfa902bf57a9d1ac429d60905
zhangkun@boxfish.cn <194264514@qq.com>
2019-07-12 20:39:19 +0800

    之后补文档

--- pages/index/index.js
+++ pages/index/index.js
@@ -8,45 +8,45 @@ Page({
 
         // 电视
         network.getMoviesList({
-                success: function(movies) {
-                    that.setData({
-                        movies: movies
-                    })
-                },
-                fail: function(msg) {
-                    console.log(msg);
-                },
-                complete: function(msg) {
-                    console.log("首页电影网路回调 " + msg);
-                }
-            }),
+            success: function(movies) {
+                that.setData({
+                    movies: movies
+                })
+            },
+            fail: function(msg) {
+                console.log(msg);
+            },
+            complete: function(msg) {
+                console.log("首页电影网路回调 " + msg);
+            }
+        })
 
-            network.getTvsList({
-                success: function(tvs) {
-                    that.setData({
-                        tvs: tvs
-                    })
-                },
-                fail: function(msg) {
-                    console.log(msg);
-                },
-                complete: function(msg) {
-                    console.log("首页电视网路回调 " + msg);
-                },
-            }),
+        network.getTvsList({
+            success: function(tvs) {
+                that.setData({
+                    tvs: tvs
+                })
+            },
+            fail: function(msg) {
+                console.log(msg);
+            },
+            complete: function(msg) {
+                console.log("首页电视网路回调 " + msg);
+            },
+        })
 
-            network.getArtList({
-                success: function(shows) {
-                    that.setData({
-                        shows: shows
-                    })
-                },
-                fail: function(msg) {
-                    console.log(msg);
-                },
-                complete: function(msg) {
-                    console.log("首页电视综艺回调 " + msg);
-                },
-            })
+        network.getArtList({
+            success: function(shows) {
+                that.setData({
+                    shows: shows
+                })
+            },
+            fail: function(msg) {
+                console.log(msg);
+            },
+            complete: function(msg) {
+                console.log("首页电视综艺回调 " + msg);
+            },
+        })
     }
 })
---
+++ pages/list/list.js
@@ -0,0 +1,82 @@
+// pages/list/list.js
+import { network } from "../index/network.js"
+
+Page({
+
+    /**
+     * 页面的初始数据
+     */
+    data: {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面加载
+     */
+    onLoad: function(options) {
+        console.log("获取数据");
+        var that = this;
+        network.getMoviesList({
+            success: function(movies) {
+                that.setData({
+                    movies: movies
+                })
+            },
+            fail: function(msg) {
+                console.log(msg);
+            },
+            complete: function(msg) {
+                console.log("首页电影网路回调 " + msg);
+            }
+        })
+    },
+
+    /**
+     * 生命周期函数--监听页面初次渲染完成
+     */
+    onReady: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面显示
+     */
+    onShow: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面隐藏
+     */
+    onHide: function() {
+
+    },
+
+    /**
+     * 生命周期函数--监听页面卸载
+     */
+    onUnload: function() {
+
+    },
+
+    /**
+     * 页面相关事件处理函数--监听用户下拉动作
+     */
+    onPullDownRefresh: function() {
+
+    },
+
+    /**
+     * 页面上拉触底事件的处理函数
+     */
+    onReachBottom: function() {
+
+    },
+
+    /**
+     * 用户点击右上角分享
+     */
+    onShareAppMessage: function() {
+
+    }
+})
